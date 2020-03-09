6491c340d293bf405938530611d84a861853e595
Peter Lin <peterlin@Peters-Work-Laptop.local>
2020-03-09 10:33:22 +0800

    [modify] add migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Earn = artifacts.require("./Earn.sol");
+const NoobCoin = artifacts.require("./NoobCoin.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Earn);
+  deployer.deploy(NoobCoin);
 };
