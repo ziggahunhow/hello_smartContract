a24226ec62b770c9e8bedf6cfe322d46008be74f
Peter Lin <szul@uw.edu>
2020-03-11 21:18:49 +0800

    [modify] add new Earn

---
+++ contracts/newEarn.sol
@@ -0,0 +1,36 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+contract Erc20 {
+    function approve(address, uint256) external returns (bool);
+    function transfer(address, uint256) external returns (bool);
+}
+
+contract CErc20 {
+    function mint(uint256) external returns (uint256);
+}
+
+contract NewEarn {
+    function sendTouch(uint256 erc20Amount) public returns (uint256) {
+        uint256 _touch;
+        return _touch;
+    }
+    function supplyErc20ToCompound(
+        address _erc20Contract,
+        address _cErc20Contract,
+        uint256 _numTokensToSupply
+    ) public returns (uint256) {
+        // Create a reference to the underlying asset contract, like DAI.
+        Erc20 underlying = Erc20(_erc20Contract);
+
+        // Create a reference to the corresponding cToken contract, like cDAI
+        CErc20 cToken = CErc20(_cErc20Contract);
+
+        // Approve transfer on the ERC20 contract
+        underlying.approve(_cErc20Contract, _numTokensToSupply);
+
+        // Mint cTokens and return the result
+        uint256 mintResult = cToken.mint(_numTokensToSupply);
+        sendTouch(_numTokensToSupply);
+        return mintResult;
+    }
+}
